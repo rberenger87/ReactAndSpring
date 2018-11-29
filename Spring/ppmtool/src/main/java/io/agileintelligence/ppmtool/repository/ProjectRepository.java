aabce8c0946fbf4f9e26d1ec9b6db1a1ca9854a9
Renan Berenger <RENANB@amdocs.com>
2018-11-29 15:31:18 -0200

    Project Controller

---
+++ Spring/ppmtool/src/main/java/io/agileintelligence/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.ppmtool.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import io.agileintelligence.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> 
+{
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> iterable);
+}
