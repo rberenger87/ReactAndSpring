20fe1e882a103d33c7c607b496d561b6bf291642
Renan <rberenger@gmail.com>
2020-04-14 13:10:07 -0300

    Project Not Found

---
+++ Spring/ppmtool/src/main/java/io/agileintelligence/ppmtool/exception/ProjectNotFoundException.java
@@ -0,0 +1,15 @@
+package io.agileintelligence.ppmtool.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ProjectNotFoundException extends RuntimeException {
+
+	public ProjectNotFoundException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
